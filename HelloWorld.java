807f4db75973c7547050fe87d2294cddcb52978c
Þórunn <thorunn.thorarins@gmail.com>
2016-03-09 17:06:47 +0000

    ekkert gert

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World!!!!! þessu er breytt héraaaaaaaaaaaaa");
+		System.out.println("megasegagaman")
 	}
 }
 
