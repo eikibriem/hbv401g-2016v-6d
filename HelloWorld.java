6057e39a360fe80ea672e9dd5671f24358070c80
Þórunn <thorunn.thorarins@gmail.com>
2016-03-09 14:08:42 +0000

    blaaaa

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,9 @@
 
-public class HelloWorld {
-	public static void main(String[] args) {
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
 		System.out.println("Hello World");
+		System.out.println("Þetta er svo gaman!");
 	}
 }
