ed5c419f3124d52f6d8e290b3baa23756dfda792
Þórunn <thorunn.thorarins@gmail.com>
2016-04-03 18:22:38 -0400

    added tests for Tours class

--- Search.java
+++ Search.java
@@ -3,7 +3,7 @@
  */
 public class Search
 {
-    private String[] type = {"adventure", "citywalk", "horseriding", "hiking",  "sightseeing"};
+    private String[] type = {"adventure", "city walk", "horseriding", "hiking",  "sightseeing"};
     private String[] difficulty = {"easy", "medium", "hard"};
     private String[] area = {"Capital region", "east", "north", "south", "west"};
     private String[] language = {"danish", "english", "german", "icelandic", "spanish"};
